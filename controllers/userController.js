d5a41f2cdffe5a14acfc105c65c42db2276ba469
Zahid Hasan <jahidhasann67@gmail.com>
2023-08-15 23:40:47 +0600

    added error handling

--- controllers/userController.js
+++ controllers/userController.js
@@ -145,41 +145,101 @@ const signUp = async (req, res) => {
   });
 };
 
+// const signIn = async (req, res) => {
+//   let student = {};
+//   try {
+//     validator(req.body, "signin");
+//     let user = "";
+//     let isValid = true;
+//     if (isEmail(req.body.email.toLowerCase())) {
+//       user = await User.findOne({ email: req.body.email.toLowerCase(), role: "admin" });
+//       if (isEmpty(user))
+//         user = await User.findOne({ email: req.body.email.toLowerCase(), role: "teacher" });
+//     } else {
+//       user = await User.findOne({ id: req.body.email.toUpperCase() });
+//       student = await Student.findOne({ id: req.body.email.toUpperCase() });
+//       isValid = (student?.isValid || false) && (student?.isEnrolled || false);
+//     }
+//     if (isEmpty(user))
+//       throw customError({
+//         code: 404,
+//         message: "User not found",
+//       });
+  
+//     if (!isValid)
+//       throw customError({
+//         code: 404,
+//         message: "Unauthorize access.",
+//       });
+  
+//     const validPass = await bcrypt.compare(req.body.password, user.password);
+//     if (!validPass)
+//       throw customError({
+//         code: 403,
+//         message: "Wrong password",
+//       });
+//       console.log("Hiii", student);
+//       console.log("pro", student?.profession)
+//     const token = jwt.sign(
+//       {
+//         id: user.id,
+//         email: user.email,
+//         role: user.role,
+//         courseId: student?.courseId || null,
+//         profession: student?.profession || null,
+//         type: "auth",
+//       },
+//       variables.jwtSecret,
+//       {
+//         expiresIn: "12h",
+//       }
+//     );
+  
+//     res.status(200).send({
+//       message: "User signed in successfully",
+//       token: token,
+//     });
+//   } catch (err) {
+//     res.status(err.code).send({
+//       message: err.message,
+//     });
+//   }
+  
+// };
+
 const signIn = async (req, res) => {
-  let student = {};
-  try {
     validator(req.body, "signin");
-    let user = "";
+    
+    let user = null;
+    let student = null;
     let isValid = true;
+    
     if (isEmail(req.body.email.toLowerCase())) {
-      user = await User.findOne({ email: req.body.email.toLowerCase(), role: "admin" });
-      if (isEmpty(user))
-        user = await User.findOne({ email: req.body.email.toLowerCase(), role: "teacher" });
+      user = await User.findOne({ email: req.body.email.toLowerCase(), role: { $in: ["admin", "teacher"] } });
     } else {
       user = await User.findOne({ id: req.body.email.toUpperCase() });
       student = await Student.findOne({ id: req.body.email.toUpperCase() });
-      isValid = (student?.isValid || false) && (student?.isEnrolled || false);
+      isValid = student?.isValid && student?.isEnrolled;
     }
-    if (isEmpty(user))
+    if (!user) {
       throw customError({
         code: 404,
         message: "User not found",
       });
-  
-    if (!isValid)
+    }
+    if (!isValid) {
       throw customError({
-        code: 404,
-        message: "Unauthorize access.",
+        code: 403,
+        message: "Unauthorized access.",
       });
-  
+    }
     const validPass = await bcrypt.compare(req.body.password, user.password);
-    if (!validPass)
+    if (!validPass) {
       throw customError({
         code: 403,
         message: "Wrong password",
       });
-      console.log("Hiii", student);
-      console.log("pro", student?.profession)
+    }
     const token = jwt.sign(
       {
         id: user.id,
@@ -194,16 +254,12 @@ const signIn = async (req, res) => {
         expiresIn: "12h",
       }
     );
-  
     res.status(200).send({
       message: "User signed in successfully",
       token: token,
     });
-  } catch (err) {
-    console.log("hiii", err);
-  }
-  
 };
+
 
 const changePassword = async (req, res) => {
   const id = req.body.id.toUpperCase();
