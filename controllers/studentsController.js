ca4af53d4ad4e41992164123126246e0e012c194
salmansrabon <salmansrabon@gmail.com>
2024-01-08 15:50:30 +0600

    merge with main

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -388,8 +388,6 @@ const addAttandence_Admin = async (req, res) => {
       state: true,
     },
   });
-
-  // console.log(student.attendances)
 };
 const addQuizAnswer = async (req, res) => {
   const { id } = req.params;
